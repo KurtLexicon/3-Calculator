96148e1530d28019a57611b1bcfc0f00095967b2
KurtLexicon <107879424+KurtLexicon@users.noreply.github.com>
2022-07-08 12:18:55 +0200

    Remove unused files Coordinate oroject names and direcory names

--- CalculatorConsole/Program.cs
+++ CalculatorConsole/Program.cs
@@ -1,44 +1,34 @@
-﻿
-
-
+﻿// See https://aka.ms/new-console-template for more information
 using CalculatorClasses;
 
-namespace CalculatorConsole
-{
-    internal class Program
-    {
-        static string welcomeString =
+string welcomeString =
 @"==============================================================
 Give an expression to calculate.
 The expression can contain any of these operators: + - * /
-example: 4 - 6 + 3.2 * 1,5 / 2
+Example: 4 - 6 + 3.2 * 1,5 / 2
 Parantheses are not supported.
 Exit the program by giving an empty input (just press ENTER).
 ==============================================================";
 
-        static void Main(string[] args)
-        {
-            while (true)
-            {
-                Console.WriteLine(welcomeString);
-                string strExpression = Console.ReadLine() ?? "";
-                if (string.IsNullOrEmpty(strExpression)) break;
-                try
-                {
-                    Expression expression = Expression.ParseExpression(strExpression);
-                    Console.WriteLine(expression.StructuredString());
-                    double result = expression.Calculate();
-                    Console.WriteLine($"{expression} = {result}");
-                }
-                // catch (ParseException ex) { Console.WriteLine(ex.Message); }
-                catch (DivideByZeroException ex) { Console.WriteLine(ex.Message); }
-                catch (Exception ex) {
-                    string msg = $"An unexpected error occurred:\n\n{ex.Message}";
-                    Console.WriteLine(msg);
-                }
-                Console.WriteLine();
-            }
-        }
+while (true)
+{
+    Console.WriteLine(welcomeString);
+    string strExpression = Console.ReadLine() ?? "";
+    if (string.IsNullOrEmpty(strExpression)) break;
 
+    try
+    {
+        Expression expression = Expression.ParseExpression(strExpression);
+        double result = expression.Calculate();
+        Console.WriteLine($"{expression} = {result}");
     }
+    catch (ParseException ex) { Console.WriteLine(ex.Message); }
+    catch (DivideByZeroException ex) { Console.WriteLine(ex.Message); }
+    catch (Exception ex)
+    {
+        string msg = $"An unexpected error occurred:\n{ex}";
+        Console.WriteLine(msg);
+    }
+
+    Console.WriteLine();
 }
